3426907a9a12e5e0d8cf0d5455adb892fc0fd736
Alexander Mayer <alexander.mayer.a@gmail.com>
2026-01-08 11:14:15 +0100

    debugging db-integration

--- app/src/test/java/com/project/back_end/BackEndApplicationTests.java
+++ app/src/test/java/com/project/back_end/BackEndApplicationTests.java
@@ -2,12 +2,16 @@ package com.project.back_end;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Lädt application-test.properties mit Platzhaltern
 class BackEndApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Prüft nur, ob der Spring ApplicationContext korrekt startet
+        // Die DB-Verbindung wird über Umgebungsvariablen konfiguriert
+    }
 
 }
